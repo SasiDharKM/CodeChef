b09f6bfc287fab23588eb2216b1787e41648aa3d
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-12 00:32:47 +0530

    Added solution to chefrout prob

---
+++ CHEFROUT.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,check;
+	long long int len,i;
+	char s[100000];
+	
+	cin>>n;
+	while(n--) {
+		cin>>s;
+		len = strlen(s);
+		check = 1;
+		for( i = 0; i<len-1 && check==1; i++) {
+			if(s[i]-s[i+1] > 0)
+				check = 0;
+		}
+
+		if(check)
+			cout<<"yes\n";
+		else
+			cout<<"no\n";
+	}
+	return 0;
+}
